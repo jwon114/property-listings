52a8714ed9486080efafd2b9d810ec1b2956151f
jwon114 <jthgwong@gmail.com>
2020-05-31 21:42:54 +0100

    Updates

---
+++ precache-manifest.5f049bb25d305821c95680de57da3d48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1406994f1f76832378588cfddaddad1",
+    "url": "/property-listings/index.html"
+  },
+  {
+    "revision": "f33617fa67b5befb713c",
+    "url": "/property-listings/static/css/main.5290c46b.chunk.css"
+  },
+  {
+    "revision": "e6fa017a2aa62869b707",
+    "url": "/property-listings/static/js/2.d191ff74.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/property-listings/static/js/2.d191ff74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33617fa67b5befb713c",
+    "url": "/property-listings/static/js/main.78f80a90.chunk.js"
+  },
+  {
+    "revision": "6f3f2d1494f239f46b89",
+    "url": "/property-listings/static/js/runtime-main.d1c15f0b.js"
+  }
+]);
